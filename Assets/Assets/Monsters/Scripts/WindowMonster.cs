8ca3b9d23f31ae0c7ed71467e0bfbe4ebbbc383c
fotik333 <nobelisfm@mail.ru>
2023-07-27 16:03:42 +0300

    fighting logic proto

---
+++ Assets/Assets/Monsters/Scripts/WindowMonster.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using DigitalRuby.Tween;
+using UnityEngine;
+
+public class WindowMonster : AbstractMonster {
+
+    [SerializeField]
+    private GameObject window;
+    
+    [SerializeField]
+    private GameObject monster;
+    
+    private float _startWindowY = 0;
+    private float _startMonsterY = 0;
+
+    private void Start() {
+        _startWindowY = window.transform.localPosition.y;
+        _startMonsterY = monster.transform.localPosition.y;
+    }
+
+    protected override void ProceedUpdate(float progress) {
+        if (progress < _harbingerThreshold) {
+            window.transform.localPosition = new Vector3(window.transform.localPosition.x, _startWindowY + (progress / _harbingerThreshold), 0);
+        }
+
+        if (progress >= _harbingerThreshold) {
+            monster.transform.localPosition = new Vector3(monster.transform.localPosition.x, _startMonsterY + ((progress - _harbingerThreshold) / (1 - _harbingerThreshold)) * 1.4f, 0);
+        }
+    }
+}
---
+++ Assets/Assets/Runes/Scripts/RunesStrikeContainer.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using DigitalRuby.Tween;
+using UnityEngine;
+
+public class RunesStrikeContainer : MonoBehaviour {
+    
+    [NonSerialized]
+    public bool collided = false;
+
+    private Collider _collider;
+    private List<GameObject> runes = new List<GameObject>();
+
+    private void Start() {
+        _collider = GetComponent<Collider>();
+    }
+
+    public void Strike() {
+        foreach (Transform child in transform) {
+            runes.Add(child.gameObject);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other) {
+        if (collided) return;
+        if (other.CompareTag("Player")) return;
+        
+        collided = true;
+        GetComponent<Collider>().enabled = false;
+        
+        foreach (var rune in runes) {
+            var tween = TweenFactory.Tween(
+                null,
+                0,
+                1,
+                0.5f,
+                TweenScaleFunctions.Linear,
+                (t) => {
+                    rune.GetComponent<Renderer>().material.SetFloat("_Alpha", 1 - t.CurrentProgress);
+                });
+        }
+        
+        if (other.transform.CompareTag("Enemy")) {
+            var monster = other.transform.GetComponent<AbstractMonster>();
+            
+            if (monster.IsSpawned()) {
+                monster.Damage();
+        
+                foreach (var rune in runes) {
+                    rune.GetComponentInChildren<ParticleSystem>().Play();
+                }
+            }
+        }
+    }
+}
