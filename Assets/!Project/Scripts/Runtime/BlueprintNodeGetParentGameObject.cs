37cb386805978fb969c03b9c605408664bad12e2
Valentin Marinchenko <v.marinchenko@siridar.com>
2024-04-28 16:10:19 +0400

    Fix some bp

--- Assets/!Project/Scripts/Runtime/BlueprintNodeGetParentGameObject.cs
+++ Assets/!Project/Scripts/Runtime/BlueprintNodeGetParentGameObject.cs
@@ -14,14 +14,8 @@ namespace MisterGames.BlueprintLib {
         BlueprintSources.ICloneable { }
 
     [Serializable]
-    [BlueprintNode(Name = "Get parent GameObject", Category = "GameObject", Color = BlueprintColors.Node.Data)]
+    [BlueprintNode(Name = "Get Component GameObject", Category = "GameObject", Color = BlueprintColors.Node.Data)]
     public struct BlueprintNodeGetParentGameObject : IBlueprintNode, IBlueprintOutput<GameObject> {
-
-        private Component _component;
-
-        // public void OnStart(IBlueprint blueprint, NodeToken token) {
-        //     _component = blueprint.Read(token, 0, _component);
-        // }
         
         public void CreatePorts(IBlueprintMeta meta, NodeId id) {
             meta.AddPort(id, Port.Input<Component>());
@@ -29,13 +23,10 @@ namespace MisterGames.BlueprintLib {
         }
 
         public GameObject GetPortValue(IBlueprint blueprint, NodeToken token, int port) {
-            if (port == 1) {
-                var component = blueprint.Read(token, 0, _component);
-                
-                return component.gameObject;
-            }
-
-            return default;
+            if (port != 1) return default;
+            
+            var component = blueprint.Read<Component>(token, 0);
+            return component == null ? default : component.gameObject;
         }
     }
 
