6d14e3ccc1a1fd70080d054933a0fa55145f15ee
fotik333 <47943885+fotik333@users.noreply.github.com>
2024-05-17 02:32:22 +0300

    Level 4 and 6 upd

---
+++ Assets/!Project/Blueprints/Common/ActionOnEnableDisable.cs
@@ -0,0 +1,31 @@
+﻿using System.Threading;
+using Cysharp.Threading.Tasks;
+using MisterGames.Actors;
+using MisterGames.Actors.Actions;
+using MisterGames.Collisions.Triggers;
+using MisterGames.Common.Attributes;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+namespace MisterGames.Character.Motion {
+
+    public sealed class ActionOnEnableDisable : MonoBehaviour {
+        [SerializeReference] [SubclassSelector] private IActorAction _enableAction;
+        [SerializeReference] [SubclassSelector] private IActorAction _disableAction;
+
+        private CancellationTokenSource _enableCts;
+
+        private void OnEnable() {
+            _enableCts?.Cancel();
+            _enableCts?.Dispose();
+            _enableCts = new CancellationTokenSource();
+            
+            _enableAction.Apply(null, _enableCts.Token).Forget();
+        }
+
+        private void OnDisable() {
+            _disableAction.Apply(null, _enableCts.Token).Forget();
+        }
+    }
+
+}
---
+++ Assets/!Project/Scripts/Runtime/ForestMonster.cs
@@ -0,0 +1,88 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using LitMotion;
+using MisterGames.Character.Core;
+using MisterGames.Common.Attributes;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class ForestMonster : MonoBehaviour {
+    [SerializeField] private GameObject head;
+    [SerializeField] private ParticleSystem body;
+    [SerializeField] private ParticleSystem death;
+    [SerializeField] private Light light;
+    [SerializeField] private float stopDistance = 20f;
+    
+    public event Action Stopped = delegate {};
+    
+    private Transform _transform;
+    private NavMeshAgent _agent;
+
+    private bool _stopped;
+    
+    void Start() {
+        _agent = GetComponent<NavMeshAgent>();
+    }
+
+    private void OnEnable() {
+        _transform = CharacterAccessRegistry.Instance.GetCharacterAccess().GetComponent<Transform>();
+    }
+
+    [Button]
+    void Stop() {
+        Stopped.Invoke();
+        
+        _agent.isStopped = true;
+        _stopped = true;
+
+        death.Play();
+        body.Stop(true, ParticleSystemStopBehavior.StopEmitting);
+
+        var startScale = head.transform.localScale;
+        var startRange = light.range;
+        
+        LMotion.Create(1f, 0f, 3f).Bind(value => {
+            head.transform.localScale = startScale * value;
+            light.range = startRange * value;
+        });
+    }
+
+    void Update() {
+        if (!_transform || _stopped) return;
+        
+        var position = _transform.position;
+        
+        _agent.destination = position;
+        
+        var dir = position - transform.position + head.transform.position;
+        var rot = Quaternion.LookRotation(dir);
+        head.transform.rotation = Quaternion.Lerp(transform.rotation, rot, 5 * Time.deltaTime);
+
+        var distance = GetPathRemainingDistance(_agent);
+
+        // if (_agent.velocity != Vector3.zero) {
+        //     _agent.transform.eulerAngles = new Vector3(0, Quaternion.LookRotation(_agent.velocity).eulerAngles.y, 0);            
+        // }
+
+        if (distance > stopDistance && distance < 10000f) {
+            Stop();
+        }
+    }
+
+    private static float GetPathRemainingDistance(NavMeshAgent navMeshAgent) {
+        if (navMeshAgent.pathPending ||
+            navMeshAgent.pathStatus == NavMeshPathStatus.PathInvalid ||
+            navMeshAgent.path.corners.Length == 0)
+            return -1f;
+
+        var distance = 0.0f;
+        
+        for (var i = 0; i < navMeshAgent.path.corners.Length - 1; ++i) {
+            var path = navMeshAgent.path;
+            distance += Vector3.Distance(path.corners[i], path.corners[i + 1]);
+        }
+
+        return distance;
+    }
+}
