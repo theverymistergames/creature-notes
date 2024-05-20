2c42979cb90516f493c6332ddc2565231ccf848e
fotik333 <47943885+fotik333@users.noreply.github.com>
2024-05-20 19:27:02 +0300

    Level 6 music box logic upd

---
+++ Assets/!Project/Acions/ActionOnInteract.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Threading;
+using Cysharp.Threading.Tasks;
+using MisterGames.Actors;
+using MisterGames.Actors.Actions;
+using MisterGames.Character.Core;
+using MisterGames.Collisions.Triggers;
+using MisterGames.Common.Attributes;
+using MisterGames.Interact.Interactives;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+namespace MisterGames.Character.Motion {
+
+    public sealed class ActionOnInteract : MonoBehaviour {
+        [SerializeReference] [SubclassSelector] private IActorAction _action;
+
+        private Interactive _interactive;
+        private CancellationTokenSource _enableCts;
+        private IActor _context;
+
+        private void OnEnable() {
+            _interactive = GetComponent<Interactive>();
+            _interactive.OnStartInteract += InteractiveOnOnStartInteract;
+        }
+
+        private void InteractiveOnOnStartInteract(IInteractiveUser obj) {
+            _enableCts?.Cancel();
+            _enableCts?.Dispose();
+            _enableCts = new CancellationTokenSource();
+            _context = CharacterAccessRegistry.Instance.GetCharacterAccess();
+            
+            _action?.Apply(_context, _enableCts.Token).Forget();
+        }
+
+        private void OnDisable() {
+            _interactive.OnStartInteract -= InteractiveOnOnStartInteract;
+        }
+    }
+
+}
