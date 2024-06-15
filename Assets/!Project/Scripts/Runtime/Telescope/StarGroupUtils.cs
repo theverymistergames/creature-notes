c4a348f207f01b1a926b2a6ec6cfa32bdb05464c
Valentin Marinchenko <v.marinchenko@siridar.com>
2024-06-15 22:51:34 +0400

    Telescope star lens placement

---
+++ Assets/!Project/Scripts/Runtime/Telescope/StarGroupUtils.cs
@@ -0,0 +1,37 @@
+﻿using MisterGames.Common.GameObjects;
+using UnityEngine;
+
+namespace _Project.Scripts.Runtime.Telescope {
+    
+    public static class StarGroupUtils {
+    
+        public static TransformData GetTransformDataPlacedInCenter(
+            TransformData initialData,
+            Quaternion rotationOffset,
+            float scale,
+            float groupScale,
+            Vector3 groupCenter) 
+        {
+            return new TransformData(
+                rotationOffset * (initialData.position - groupCenter) * groupScale,
+                initialData.rotation * Quaternion.Euler(0f, 0f, rotationOffset.eulerAngles.z),
+                initialData.scale * scale
+            );
+        }
+
+        public static Vector3 GetStarGroupCenter(TransformData[] objects) {
+            var center = Vector3.zero;
+                
+            for (int j = 0; j < objects.Length; j++) {
+                center += objects[j].position;
+            }
+
+            if (objects.Length > 0) {
+                center /= objects.Length;
+            }
+
+            return center;
+        }
+    }
+    
+}
---
+++ Assets/!Project/Scripts/Runtime/Telescope/StarGroupsData.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using MisterGames.Common.GameObjects;
+using UnityEngine;
+
+namespace _Project.Scripts.Runtime.Telescope {
+
+    [CreateAssetMenu(fileName = nameof(StarGroupsData), menuName = "MisterGames/Telescope" + nameof(StarGroupsData))]
+    public sealed class StarGroupsData : ScriptableObject {
+        
+        public Transform starPrefab;
+        [Min(0f)] public float detectionAngle;
+        [Min(0f)] public float detectionTime;
+        public StarGroup[] starGroups;
+
+        [Serializable]
+        public struct StarGroup {
+            [Header("Canvas Settings")]
+            public Vector3 canvasRotation;
+            public float canvasScale;
+            public TransformData[] stars;
+            public TransformData[] links;
+
+            [Header("Placement Settings")]
+            public float starScale;
+            public float groupScale;
+            public float telescopeDistance;
+            public Vector3 telescopeOrientation;
+        }
+    }
+    
+}
