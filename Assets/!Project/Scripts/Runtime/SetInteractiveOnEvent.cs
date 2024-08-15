7bb7ec70751bc116926b74a5746d7ee9febe9c51
Valentin Marinchenko <v.marinchenko@siridar.com>
2024-08-15 22:15:17 +0700

    SetInteractive upd

--- Assets/!Project/Scripts/Runtime/SetInteractiveOnEvent.cs
+++ Assets/!Project/Scripts/Runtime/SetInteractiveOnEvent.cs
@@ -1,15 +1,7 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.Threading;
-using MisterGames.Interact.Detectables;
-using MisterGames.Interact.Interactives;
 using MisterGames.Scenario.Events;
-using Unity.VisualScripting;
 using UnityEngine;
-using Debug = System.Diagnostics.Debug;
 
-public class SetInteractiveOnEvent : MonoBehaviour, IEventListener {
+public sealed class SetInteractiveOnEvent : MonoBehaviour, IEventListener {
 
     [SerializeField] private EventReference enableEvent;
     [SerializeField] private EventReference disableEvent;
@@ -26,10 +18,18 @@ public class SetInteractiveOnEvent : MonoBehaviour, IEventListener {
         if (disableOnStart) _collider.enabled = false;
     }
 
+    private void OnDestroy() {
+        enableEvent.Unsubscribe(this);
+        disableEvent.Unsubscribe(this);
+    }
+
     public void OnEventRaised(EventReference e) {
         if (e.EventId == enableEvent.EventId) {
             _collider.enabled = true;
-        } else if (e.EventId == disableEvent.EventId) {
+            return;
+        }
+
+        if (e.EventId == disableEvent.EventId) {
             _collider.enabled = false;
         }
     }
