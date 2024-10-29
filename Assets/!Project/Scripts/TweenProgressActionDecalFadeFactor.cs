ea490ca09178c03eba9f2d701bd18c742deba4ba
fotik333 <47943885+fotik333@users.noreply.github.com>
2024-10-29 21:14:12 +0300

    Base meteor logic

---
+++ Assets/!Project/Scripts/TweenProgressActionDecalFadeFactor.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using MisterGames.Tweens;
+using UnityEngine;
+using UnityEngine.Rendering.HighDefinition;
+
+namespace MisterGames.TweenLib {
+    [Serializable]
+    public sealed class TweenProgressActionDecalFadeFactor : ITweenProgressAction {
+        public DecalProjector projector;
+        public Vector2 fromTo;
+
+        public void OnProgressUpdate(float progress) {
+            projector.fadeFactor = Mathf.Lerp(fromTo.x, fromTo.y, progress);
+        }
+    }
+}
