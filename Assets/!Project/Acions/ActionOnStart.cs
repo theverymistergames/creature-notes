f70d20a222d0ce5246bba46cd2b47a4e4fdc69a4
fotik333 <47943885+fotik333@users.noreply.github.com>
2024-11-02 01:51:13 +0300

    Runaway reload base

---
+++ Assets/!Project/Acions/ActionOnStart.cs
@@ -0,0 +1,27 @@
+﻿using System.Threading;
+using Cysharp.Threading.Tasks;
+using MisterGames.Actors;
+using MisterGames.Actors.Actions;
+using MisterGames.Character.Core;
+using MisterGames.Common.Attributes;
+using UnityEngine;
+
+namespace MisterGames.Character.Motion {
+
+    public sealed class ActionOnStart : MonoBehaviour {
+        [SerializeReference] [SubclassSelector]
+        private IActorAction _action;
+
+        private CancellationTokenSource _cts;
+        private IActor _context;
+
+        private void Start() {
+            _cts?.Cancel();
+            _cts?.Dispose();
+            _cts = new CancellationTokenSource();
+
+            _context = CharacterSystem.Instance.GetCharacter();
+            _action?.Apply(_context, _cts.Token).Forget();
+        }
+    }
+}
---
+++ Assets/!Project/Acions/ActionTeleportRigidbody.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Threading;
+using Cysharp.Threading.Tasks;
+using MisterGames.Actors;
+using MisterGames.Actors.Actions;
+using UnityEngine;
+
+namespace MisterGames.Character.Motion {
+    [Serializable]
+    public sealed class ActionTeleportRigidbody : IActorAction {
+        [SerializeField] private Rigidbody body;
+        [SerializeField] private Transform transform;
+
+        public UniTask Apply(IActor context, CancellationToken cancellationToken = default) {
+            body.angularVelocity = Vector3.zero;
+            body.linearVelocity = Vector3.zero;
+            
+            body.rotation = transform.rotation;
+            body.position = transform.position;
+
+            return default;
+        }
+    }
+}
