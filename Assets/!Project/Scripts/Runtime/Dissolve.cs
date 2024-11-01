c30f1def19d08dfe3b79a69712571fbc8281008c
Valentin Marinchenko <109593086+theverymistergames@users.noreply.github.com>
2024-11-01 22:49:03 +0300

    Build updates

--- Assets/!Project/Scripts/Runtime/Dissolve.cs
+++ Assets/!Project/Scripts/Runtime/Dissolve.cs
@@ -1,11 +1,7 @@
-using System;
 using System.Collections.Generic;
 using System.Linq;
 using LitMotion;
-using MisterGames.Common.Attributes;
-using MisterGames.Common.Layers;
 using MisterGames.Interact.Detectables;
-using UnityEditor.UIElements;
 using UnityEngine;
 
 public class Dissolve : MonoBehaviour {
