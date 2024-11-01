9de24453ca0c59731f7495a65ba17eea045a123c
fotik333 <47943885+fotik333@users.noreply.github.com>
2024-11-02 02:07:01 +0300

    Car teleport transition & death from meteor

---
+++ Assets/!Project/Scripts/TweenProgressActionUIElementOpacity.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using MisterGames.Tweens;
+using UnityEngine;
+using UnityEngine.Rendering.HighDefinition;
+using UnityEngine.UI;
+
+namespace MisterGames.TweenLib {
+    [Serializable]
+    public sealed class TweenProgressActionUIElementOpacity : ITweenProgressAction {
+        public Vector2 fromTo;
+        public Graphic graphic;
+
+        public void OnProgressUpdate(float progress) {
+            var color = graphic.color;
+            
+            color.a = Mathf.Lerp(fromTo.x, fromTo.y, progress);;
+            
+            graphic.color = color;
+        }
+    }
+}
