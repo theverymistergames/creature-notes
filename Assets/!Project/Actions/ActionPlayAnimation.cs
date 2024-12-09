400e2f539624ecdcd80fafe4163518e566541515
fotik333 <47943885+fotik333@users.noreply.github.com>
2024-12-09 15:04:41 +0300

    Fox updated

---
+++ Assets/!Project/Actions/ActionPlayAnimation.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Threading;
+using Cysharp.Threading.Tasks;
+using MisterGames.Actors;
+using MisterGames.Actors.Actions;
+using UnityEditor.Animations;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace MisterGames.ActionLib.GameObjects {
+    [Serializable]
+    public sealed class ActionPlayAnimation : IActorAction {
+        public Animator animator;
+        public string animationName;
+        
+        public UniTask Apply(IActor context, CancellationToken cancellationToken = default) {
+            animator.Play(animationName);
+            return default;
+        }
+    }
+    
+}
