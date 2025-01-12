74e63a07dbaaa337c89a0dd48a919d3ac615b1e4
Valentin Marinchenko <v.marinchenko@siridar.com>
2025-01-12 13:46:01 +0300

    Spider web spawn animation

--- Assets/!Project/Scripts/Runtime/Spider/SpiderWeb.cs
+++ Assets/!Project/Scripts/Runtime/Spider/SpiderWeb.cs
@@ -3,7 +3,9 @@ using MisterGames.Collisions.Utils;
 using MisterGames.Common.Attributes;
 using MisterGames.Common.Maths;
 using MisterGames.Common.Pooling;
+using MisterGames.Common.Tick;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 #if UNITY_EDITOR
 using UnityEditor;
@@ -11,12 +13,19 @@ using UnityEditor;
 
 namespace _Project.Scripts.Runtime.Spider {
     
-    public sealed class SpiderWeb : MonoBehaviour {
+    public sealed class SpiderWeb : MonoBehaviour, IUpdate {
 
         [SerializeField] private LineRenderer _webLinePrefab;
+        
+        [Header("Spawn")]
         [SerializeField] [MinMaxSlider(0, 100)] private Vector2Int _spawnPointsCount;
         [SerializeField] [MinMaxSlider(0, 100)] private Vector2Int _raysCount;
         [SerializeField] [MinMaxSlider(0f, 2f)] private Vector2 _rayWidth;
+        [SerializeField] [Min(0f)] private float _spawnDelayMax = 1;
+        [SerializeField] [MinMaxSlider(0f, 10f)] private Vector2 _spawnDurationRange;
+        [SerializeField] private AnimationCurve _spawnScaleCurve = AnimationCurve.Linear(0f, 0f, 1f, 1f);
+        
+        [Header("Detection")]
         [SerializeField] private LayerMask _layerMask;
         [SerializeField] [Min(0f)] private float _maxSearchDistance;
         [SerializeField] [Min(0f)] private float _maxNextPointDistance;
@@ -24,6 +33,25 @@ namespace _Project.Scripts.Runtime.Spider {
         [SerializeField] [Min(1)] private int _maxHits = 6;
         [SerializeField] [Min(0)] private int _maxRetryAttempts = 20;
 
+        private readonly struct LineAnimationData {
+            
+            public readonly LineRenderer line;
+            public readonly Transform transform;
+            public readonly float startTime;
+            public readonly float endTime;
+            public readonly float width;
+            public readonly float distance;
+            
+            public LineAnimationData(LineRenderer line, float startTime, float endTime, float width, float distance) {
+                this.line = line;
+                transform = line.transform;
+                this.startTime = startTime;
+                this.endTime = endTime;
+                this.width = width;
+                this.distance = distance;
+            }
+        }
+        
         private static readonly Vector3[] Directions = {
             Vector3.up,
             Vector3.down,
@@ -32,11 +60,17 @@ namespace _Project.Scripts.Runtime.Spider {
             Vector3.forward,
             Vector3.back,
         };
-        
+
+        private readonly List<LineAnimationData> _animationList = new();
         private RaycastHit[] _hits;
         
         private void Awake() {
             _hits = new RaycastHit[_maxHits];
+        }
+
+        private void OnDisable() {
+            _animationList.Clear();
+            PlayerLoopStage.Update.Unsubscribe(this);
         }
 
         private bool PlaceWeb(Vector3 position, List<LineRenderer> dest) {
@@ -47,6 +81,8 @@ namespace _Project.Scripts.Runtime.Spider {
             
             int pointsCount = _spawnPointsCount.GetRandomInRange();
             bool spawnedAtLeastOne = false;
+            
+            _animationList.Clear();
             
             for (int i = 0; i < pointsCount; i++) {
                 if (!TryGetClosestPointOnSurface(pos, rot, out var hit) &&
@@ -62,6 +98,8 @@ namespace _Project.Scripts.Runtime.Spider {
                     ? hit.point
                     : newPos;
             }
+            
+            PlayerLoopStage.Update.Subscribe(this);
             
             return spawnedAtLeastOne;
         }
@@ -98,16 +136,45 @@ namespace _Project.Scripts.Runtime.Spider {
             
             var t = line.transform;
             t.SetPositionAndRotation(start, Quaternion.LookRotation(end - start));
-            t.localScale = new Vector3(width, width, distance);
+            t.localScale = Vector3.zero;
             
             line.useWorldSpace = false;
             line.SetPosition(0, Vector3.zero);
             line.SetPosition(1, Vector3.forward);
 
             line.startWidth = 0f;
-            line.endWidth = width;
+            line.endWidth = 0f;
+
+            float startTime = Time.time + Random.Range(0f, _spawnDelayMax);
+            float endTime = startTime + _spawnDurationRange.GetRandomInRange();
+
+            _animationList.Add(new LineAnimationData(line, startTime, endTime, width, distance));
         }
-        
+
+        void IUpdate.OnUpdate(float dt) {
+            float time = Time.time;
+            int finishedCount = 0;
+            
+            for (int i = 0; i < _animationList.Count; i++) {
+                var data = _animationList[i];
+                
+                float t = data.endTime.IsNearlyEqual(data.startTime) 
+                    ? 1f 
+                    : Mathf.Clamp01(time - data.startTime) / (data.endTime - data.startTime);
+                float p = _spawnScaleCurve.Evaluate(t);
+                
+                data.line.endWidth = Mathf.Lerp(0f, data.width, p);
+                data.transform.localScale = Vector3.Lerp(Vector3.zero, new Vector3(data.width, data.width, data.distance), p);
+                
+                if (t >= 1f) finishedCount++;
+            }
+            
+            if (finishedCount < _animationList.Count) return;
+            
+            _animationList.Clear();
+            PlayerLoopStage.Update.Unsubscribe(this);
+        }
+
         private LineRenderer CreateLine() {
 #if UNITY_EDITOR
             return PrefabUtility.InstantiatePrefab(_webLinePrefab, transform) as LineRenderer;
