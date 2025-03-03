c736e601959ea4aa6fd404b54b1b237d6d69dd5e
Valentin Marinchenko <109593086+theverymistergames@users.noreply.github.com>
2025-03-03 22:57:57 +0300

    Add zombie head for painting monster, fix fireball gravity in rb slow zone

---
+++ Assets/!Project/Scripts/Runtime/Enemies/Painting/BlendShapeController.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+namespace _Project.Scripts.Runtime.Enemies.Painting {
+    
+    public sealed class BlendShapeController : MonoBehaviour {
+        
+        [SerializeField] private SkinnedMeshRenderer _skinnedMeshRenderer;
+
+        private void Update() {
+            //_skinnedMeshRenderer.ble
+        }
+    }
+}
