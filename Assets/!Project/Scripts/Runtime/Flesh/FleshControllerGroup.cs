7accca020941c1ad866c77869d0ba9129b03a942
Valentin Marinchenko <109593086+theverymistergames@users.noreply.github.com>
2025-03-27 22:07:41 +0300

    Flesh fov progress

--- Assets/!Project/Scripts/Runtime/Flesh/FleshControllerGroup.cs
+++ Assets/!Project/Scripts/Runtime/Flesh/FleshControllerGroup.cs
@@ -1,5 +1,9 @@
 ﻿using System;
+using MisterGames.Character.Core;
+using MisterGames.Character.View;
 using MisterGames.Common.Attributes;
+using MisterGames.Common.Easing;
+using MisterGames.Common.GameObjects;
 using MisterGames.Tweens;
 using UnityEngine;
 
@@ -8,8 +12,15 @@ namespace _Project.Scripts.Runtime.Flesh {
     public sealed class FleshControllerGroup : MonoBehaviour {
 
         [SerializeField] private FleshController[] _fleshControllers;
+        [SerializeField] private GameObject[] _enableGameObjects;
         [SerializeField] [Range(0f, 1f)] private float _progress;
 
+        [Header("Camera")]
+        [SerializeField] private float _fovWeight = 1f;
+        [SerializeField] private float _fovOffset = 20f;
+        [SerializeField] private AnimationCurve _fovCurve = EasingType.Linear.ToAnimationCurve();
+        [SerializeField] private AnimationCurve _fovWeightCurve = EasingType.Linear.ToAnimationCurve();
+        
         [Header("Effects")]
         [SerializeField] private Effect[] _effects;
 
@@ -18,22 +29,63 @@ namespace _Project.Scripts.Runtime.Flesh {
             public AnimationCurve progressCurve;
             [SerializeReference] [SubclassSelector] public ITweenProgressAction progressAction;
         }
+
+        private CameraContainer _cameraContainer;
+        private bool _hasCameraState;
+        private int _cameraStateId;
         
         private void Awake() {
+            _cameraContainer = CharacterSystem.Instance.GetCharacter().GetComponent<CameraContainer>();
+            
             ApplyProgress(_progress);
         }
-        
+
+        private void OnDisable() {
+            if (!_hasCameraState) return;
+
+            _hasCameraState = false;
+            _cameraContainer.RemoveState(_cameraStateId);
+        }
+
         public void ApplyProgress(float progress) {
             _progress = progress;
+            
+            ApplyFlesh(progress);
+            ApplyEffects(progress);
+            ApplyCameraEffects(progress);
+        }
 
+        private void ApplyFlesh(float progress) {
             for (int i = 0; i < _fleshControllers.Length; i++) {
                 _fleshControllers[i].ApplyProgress(progress);
             }
+        }
 
+        private void ApplyEffects(float progress) {
+            _enableGameObjects.SetActive(progress > 0f);
+            
             for (int i = 0; i < _effects.Length; i++) {
                 ref var effect = ref _effects[i];
                 effect.progressAction?.OnProgressUpdate(effect.progressCurve.Evaluate(progress));   
             }
+        }
+
+        private void ApplyCameraEffects(float progress) {
+            if (progress <= 0f) {
+                if (_hasCameraState) _cameraContainer.RemoveState(_cameraStateId);
+                _hasCameraState = false;
+                return;
+            }
+
+            if (!_hasCameraState) {
+                _cameraStateId = _cameraContainer.CreateState();
+                _hasCameraState = true;
+            }
+            
+            float fov = _fovCurve.Evaluate(progress) * _fovOffset;
+            float weight = _fovWeightCurve.Evaluate(progress) * _fovWeight;
+            
+            _cameraContainer.SetFovOffset(_cameraStateId, weight, fov);
         }
 
 #if UNITY_EDITOR
