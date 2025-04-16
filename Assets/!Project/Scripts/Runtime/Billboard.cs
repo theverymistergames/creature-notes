69b2bc2a7eb55c363031809be02477e51f7608a2
Valentin Marinchenko <109593086+theverymistergames@users.noreply.github.com>
2025-04-16 21:17:41 +0300

    Depth post pass for fireball vfx to support DoF

--- Assets/!Project/Scripts/Runtime/Billboard.cs
+++ Assets/!Project/Scripts/Runtime/Billboard.cs
@@ -1,20 +1,36 @@
+using MisterGames.Actors;
 using MisterGames.Character.Core;
 using MisterGames.Common.Tick;
 using UnityEngine;
 
 public sealed class Billboard : MonoBehaviour, IUpdate
 {
+    private IActor _actor;
+    private Transform _camera;
+    
     private void OnEnable() {
         PlayerLoopStage.LateUpdate.Subscribe(this);
     }
 
     private void OnDisable() {
         PlayerLoopStage.LateUpdate.Unsubscribe(this);
+        
+        _actor = null;
+        _camera = null;
     }
 
     public void OnUpdate(float dt) {
-        if (CharacterSystem.Instance.GetCharacter() is not {} actor) return;
+        if (CharacterSystem.Instance.GetCharacter() is not { } actor) {
+            _actor = null;
+            _camera = null;
+            return;
+        }
+
+        if (_actor == null || _actor != actor) {
+            _actor = actor;
+            _camera = actor.GetComponent<Camera>().transform;
+        }
         
-        transform.LookAt(actor.Transform.position, Vector3.up);
+        transform.LookAt(_camera.position, Vector3.up);
     }
 }
