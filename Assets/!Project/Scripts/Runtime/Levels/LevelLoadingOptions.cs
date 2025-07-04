7773e167f8e0ea25c336d5b7ad415fb0c9089e5f
Valentin Marinchenko <109593086+theverymistergames@users.noreply.github.com>
2025-07-04 15:38:03 +0300

    Tweak GlobalVolume, disable Volumetric Clouds

--- Assets/!Project/Scripts/Runtime/Levels/LevelLoadingOptions.cs
+++ Assets/!Project/Scripts/Runtime/Levels/LevelLoadingOptions.cs
@@ -1,12 +1,12 @@
 ﻿namespace _Project.Scripts.Runtime.Levels {
     
     public static class LevelLoadingOptions {
-        
+
         /// <summary>
         /// Disabled during build to avoid opening scenes in <see cref="LevelRoot"/>.
         /// </summary>
-        public static bool AllowLevelLoadingInEditor { get; set; }
-        
+        public static bool AllowLevelLoadingInEditor { get; set; } = true;
+
     }
 
 }
